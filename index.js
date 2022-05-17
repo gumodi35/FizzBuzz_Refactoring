003b0bc3b9beebfc5d0dba535aaf541523e30f78
gumodi35 <gumodi10@gmail.com>
2022-05-17 16:36:48 -0500

    creando las carpetas y el ExplorerService.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const Reader = require("./../FIZZBUZZ/lib/utils/Reader")
 
 const explorers = Reader.readJsonFile("explorers.json")
-console.log("probando que funcione")
+//console.log("probando que funcione")

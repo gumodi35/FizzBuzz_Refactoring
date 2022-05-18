3bb13f9cd26598a8f8bbf2251441451429c82ba9
gumodi35 <gumodi10@gmail.com>
2022-05-18 18:42:07 -0500

    guardando cambios

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -6,6 +6,6 @@ describe("Tets para ExplorerService", () => {
     test("Requerimiento 1: Calcular todos los explorers en una mission determinada", ()=>{
         const explorers = [{ mission: "node" }]
         const explorersInNode = ExplorerService.filterByMission(explorers, "node");
-        expect(explorersInNode.length).toBe(1)
+        expect(explorersInNode.length).toBe(2)
     })
 })

8dc0f2b14cde6f65a57d11b608651f14ce012907
gumodi35 <gumodi10@gmail.com>
2022-05-17 22:21:38 -0500

    filterByMission completado y funcionando

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-const Reader = require("./../FIZZBUZZ/lib/utils/Reader")
+const Reader = require("./lib/utils/Reader");
+const ExplorerService = require("./lib/services/ExplorerService");
 
 const explorers = Reader.readJsonFile("explorers.json")
-//console.log("probando que funcione")
+console.log(explorers)
+
+// Clase anterior con la que obtenemos los explorers
+/* const Reader = require("./lib/utils/Reader"); */
+/* const explorers = Reader.readJsonFile("explorers.json"); */
+
+// Aplicación del ExplorerService sobre la lista de explorers
+//console.log(ExplorerService.filterByMission(explorers, "node"));
+/* console.log(ExplorerService.getAmountOfExplorersByMission(explorers, "node")); */
+/* console.log(ExplorerService.getExplorersUsernamesByMission(explorers, "node")); */
